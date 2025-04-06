b654277c516d04485916991d435dfbda05101266
dreadflopp <dreadflopp@gmail.com>
2025-04-06 21:55:50 +0200

    inital commit

---
+++ Settings.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace RenameHeavyArmoryWeapons
+{
+    public class Settings
+    {
+        // Checkbox for weapon renaming feature
+        public bool RenameWeapons { get; set; } = true;
+
+        // Checkbox for perk description updates
+        public bool UpdatePerkDescriptions { get; set; } = true;
+    }
+} 
